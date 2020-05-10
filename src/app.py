b149b56b90250c7566227c7f6b66945498728805
Arne Halvorsen <arne.halvorsen@gmail.com>
2020-05-10 03:06:28 +0200

    non functional improvement of code

--- src/app.py
+++ src/app.py
@@ -2,8 +2,9 @@
 
 import json
 import sys
-import flask
 from typing import List
+
+import flask
 
 from dewco import domain
 from dewco.systems.handlers import SystemHandlers, add_common_system_handlers
@@ -15,6 +16,23 @@ app.config["DEBUG"] = True
 systemHandlers = dict()
 add_common_system_handlers(systemHandlers)
 add_sense_hat_handlers(systemHandlers)
+
+def get_systems_from_query() -> List[str]: 
+    systemsQuery = flask.request.args.get("systems")
+    if systemsQuery == None:
+        return []
+    return systemsQuery.split(',')
+
+def get_result_json(result: object) -> str:
+    if result == None:
+        raise TypeError("result is None")
+    retVal = json.dumps(result, default=lambda x: x.__dict__, indent=4)
+    return retVal
+
+def ok(result) -> str:
+    json = get_result_json(result)
+    response = app.response_class(json, status = 200, mimetype = 'application/json')
+    return response
 
 @app.route('/system', methods=['GET'])
 def getSystem():
@@ -28,22 +46,11 @@ def getSystem():
     except:
         message = sys.exc_info()[0]
         result = domain.Result.from_error(message)
-
-    json = getResultJSON(result)
-    response = app.response_class(json, status = 200, mimetype='application/json')
-
-    return response
-
-def get_systems_from_query() -> List[str]: 
-    systemsQuery = flask.request.args.get("systems")
-    if systemsQuery == None:
-        return []
-    return systemsQuery.split(',')
+    return ok(result)
 
 @app.route('/state', methods=['GET'])
 def getState():
     system = get_systems_from_query()
-    print(system)
     result = None
     try:
         states = []
@@ -54,16 +61,6 @@ def getState():
     except:
         message = sys.exc_info()[0]
         result = domain.Result.from_error(message)
-
-    json = getResultJSON(result)
-    response = app.response_class(json, status = 200, mimetype='application/json')
-
-    return response
-
-def getResultJSON(result: object) -> str:
-    if result == None:
-        raise TypeError("result is None")
-    retVal = json.dumps(result, default=lambda x: x.__dict__, indent=4)
-    return retVal
+    return ok(result)
 
 app.run(host = '0.0.0.0', port = 8090)
