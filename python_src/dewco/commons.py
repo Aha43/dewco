6b7ee7411ee70b1d92d7ecb51e92391e1d4ae267
Arne Halvorsen <arne.halvorsen@gmail.com>
2020-06-04 19:21:25 +0200

    Started with a commons

---
+++ python_src/dewco/commons.py
@@ -0,0 +1,37 @@
+
+# Strings starts
+def is_null_or_empty(s: str) -> bool:
+    if s is None:
+        return True
+    if len(s.strip()) == 0:
+        return True
+    return False
+# Strings ends
+
+# Exceptions starts
+class Raise:
+    @staticmethod
+    def if_none_or_empty_str(v: str, name: str = None) -> None:
+        Raise.if_none_reference(v, name)
+        v = v.strip()
+        if len(str) == 0:
+            raise ValueError(Raise.__add_name("Empty str", name))
+    
+    @staticmethod
+    def if_none_reference(v, name: str = None) -> None:
+        if v == None:
+            raise ValueError(Raise.__add_name("None reference", name))
+
+    @staticmethod
+    def if_not_in_range(v, start, to, name: str = None) -> None:
+        if v < start:
+            raise ValueError(Raise.__add_name("< " + str(start), name))
+        if v >= to:
+            raise ValueError(Raise.__add_name(">= " + str(start), name))
+
+    @staticmethod
+    def __add_name(msg: str, name: str) -> str:
+        if not is_null_or_empty(name):
+            msg += " : " + name.strip()
+        return msg
+# Exceptions ends
